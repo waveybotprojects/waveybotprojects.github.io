d5ae59dc83c216da83c1a9deccf194d09e29585b
Shazyy <yarukemc@gmail.com>
2020-03-27 23:05:05 +0100

    my bad

--- yt-sub-counter/theme/akshatmittal/assets/js/base.js
+++ yt-sub-counter/theme/akshatmittal/assets/js/base.js
@@ -1,8 +1,8 @@
-var baseURL = "http://localhost/yt-sub-counter/theme/akshatmittal/";
+var baseURL = "https://livecounts.io/yt-sub-counter/theme/akshatmittal/";
 if (typeof isCustomPage == 'undefined') isCustomPage = 0;
 if (window.location.hostname.indexOf("local.akshatmittal.com") < 0) {
    //if (window.location.protocol != "https:") window.location.replace("https:" + window.location.href.substring(window.location.protocol.length));
-    if (location.hostname.indexOf("localhost") == -1) window.location.replace(baseURL + location.hash);
+    if (location.hostname.indexOf("livecounts.io") == -1) window.location.replace(baseURL + location.hash);
     if (window.top !== window.self) window.top.location.replace(window.self.location.href);
 }
 Array.prototype.shuffle = function () {
