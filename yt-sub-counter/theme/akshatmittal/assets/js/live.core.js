f09c09eb8c1b74dd2b71c355fa0a3d2aebf0d8f3
Shazyy <yarukemc@gmail.com>
2020-03-27 23:21:26 +0100

    f

--- yt-sub-counter/theme/akshatmittal/assets/js/live.core.js
+++ yt-sub-counter/theme/akshatmittal/assets/js/live.core.js
@@ -2,23 +2,18 @@ YT.live = {
     channelID: "",
     update: function () {
         $.getJSON('https://api.livecounts.io/yt_subs', function(data) {
-            $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + this.channelID + "&key=" + YT.keyManager.getKey(), function (e) {
+            $.getJSON('https://reeeeeeeeee.livecounts.io/yt_data?type=channel&part=statistics&id='+this.channelID, function (e) {
+                console.log(e, result[0])
                 var result = data.filter(x => x.cid === this.channelID);
                 if (result.length != 0) {
                     YT.updateManager.updateSubscribers(result[0].subscriberCount)
-                    YT.updateManager.updateViews(e.items[0].statistics.viewCount);
-                    YT.updateManager.updateVideos(e.items[0].statistics.videoCount);
+                    YT.updateManager.updateViews(e.statistics.viewCount);
+                    YT.updateManager.updateVideos(e.statistics.videoCount);
                 } else {
-                    YT.updateManager.updateSubscribers(e.items[0].statistics.subscriberCount);
-                    YT.updateManager.updateViews(e.items[0].statistics.viewCount);
-                    YT.updateManager.updateVideos(e.items[0].statistics.videoCount);
-                }
-            }).fail(function() {
-                $.getJSON('https://reeeeeeeeee.livecounts.io/yt_data?type=channel&part=statistics&id='+this.channelID, function(e) {
                     YT.updateManager.updateSubscribers(e.statistics.subscriberCount);
                     YT.updateManager.updateViews(e.statistics.viewCount);
                     YT.updateManager.updateVideos(e.statistics.videoCount);
-                })
+                }
             })
         })
     },
