f964c2b856d5b91ea88569c90eedc2c6a27cc5db
Shazyy <yarukemc@gmail.com>
2020-03-27 16:56:03 +0100

    embed

---
+++ assets/global/counters/js/tiktok-realtime-embed.js
@@ -0,0 +1,200 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-119417406-7');
+
+var ok;
+
+var NextStopStealingMyShitRetard = [
+	"https://cors.livecounts.io/",
+	"https://nice-cors-proxy-1.glitch.me/",
+	"https://nice-cors-proxy-2.glitch.me/",
+	"https://nice-cors-proxy-3.glitch.me/",
+	"https://nice-cors-proxy-4.glitch.me/",
+	"https://shazyy-cors-anywhere.glitch.me/",
+	"https://shazyy-cors-anywhere-1.glitch.me/",
+    "https://shazyy-cors-anywhere-2.glitch.me/",
+    "https://nextcounts-cors01.glitch.me/",
+    "https://nextcounts-cors02.glitch.me/",
+    "https://nextcounts-cors03.glitch.me/",
+    "https://nextcounts-cors04.glitch.me/",
+    "https://nextcounts-cors05.glitch.me/",
+    "https://nextcounts-cors06.glitch.me/",
+    "https://nextcounts-cors07.glitch.me/",
+    "https://nextcounts-cors08.glitch.me/",
+    "https://nextcounts-cors09.glitch.me/",
+    "https://nextcounts-cors10.glitch.me/",
+    "https://nextcounts-cors11.glitch.me/",
+	"https://nextcounts-cors12.glitch.me/",
+	"https://corsanywherebinus.glitch.me/",
+	"https://corsanywhere.glitch.me/",
+	"https://glitch-cors-anywhere.glitch.me/",
+	"https://rob--w-cors-anywhere.glitch.me/",
+	"https://rob--w-cors-anywhere-7.glitch.me/",
+	"https://rob--w-cors-anywhere-6.glitch.me/",
+	"https://cors-anyhere.glitch.me/",
+	"https://working-proxy.glitch.me/",
+	"https://cors-anywhere-15.glitch.me/",
+	"https://chriszs-cors-anywhere.glitch.me/",
+	"https://usp-cors-proxy.glitch.me/",
+	"https://lightning-meerkat.glitch.me/",
+	"https://redditpx-cors.glitch.me/",
+	"https://redditpx-cors-2.glitch.me/",
+	"https://image-cors-proxy.glitch.me/",
+	"https://no-cors.glitch.me/",
+	"https://b-b-cors-fix.glitch.me/",
+	"https://zcors.glitch.me/",
+	"https://poc-radio-proxy.glitch.me/",
+	"https://unique-shoe.glitch.me/",
+	"https://easrng-cors-anywhere.glitch.me/",
+	"https://proxyoga.glitch.me/",
+	"https://scarlet-cirrus.glitch.me/",
+	"https://rich-collard.glitch.me/",
+	"https://melodic-anorak.glitch.me/",
+	"https://mesquite-estimate.glitch.me/",
+	"https://canva-proxy.glitch.me/",
+	"https://stump-jasper.glitch.me/",
+	"https://diamond-scallop.glitch.me/",
+	"https://sapphire-plate.glitch.me/",
+	"https://branched-geese.glitch.me/",
+	"https://bony-scribe.glitch.me/",
+	"https://wiggly-van.glitch.me/",
+	"https://catkin-principal.glitch.me/",
+	"https://private-bubbler.glitch.me/",
+	"https://feather-bull-1cpvaykota.glitch.me/",
+	"https://tropical-walkover.glitch.me/",
+	"https://airy-measure.glitch.me/",
+	"https://pentagonal-paddleboat-1.glitch.me/",
+	"https://clever-aquatic-ketchup.glitch.me/",
+	"https://hail-eggplant.glitch.me/",
+	"https://iced-scallop.glitch.me/",
+	"https://moor-geranium.glitch.me/",
+	"https://shorthaired-sedum.glitch.me/",
+	"https://dog-trombone.glitch.me/",
+	"https://cotton-relation.glitch.me/",
+	"https://api.allorigins.win/get?url=",
+	"https://corsproxy.glitch.me/",
+	"https://shazyy-cors-anywhere-6.glitch.me/",
+	"https://shazyy-cors-anywhere-7.glitch.me/",
+	"https://shazyy-cors-anywhere-8.glitch.me/",
+	"https://shazyy-cors-anywhere-9.glitch.me/",
+	"https://shazyy-cors-anywhere-10.glitch.me/",
+	"https://ajar-patch-fox.glitch.me/",
+	"https://chestnut-almond-painter.glitch.me/",
+	"https://tested-brash-minnow.glitch.me/",
+	"https://candied-dazzling-handball.glitch.me/",
+	"https://mint-spice-bass.glitch.me/",
+	"https://futuristic-bald-citrus.glitch.me/",
+	"https://plausible-momentous-parrot.glitch.me/",
+	"https://loud-alpine-silver.glitch.me/",
+	"https://power-boat.glitch.me/",
+	"https://fresh-alert-opera.glitch.me/",
+	"https://stingy-button-cowbell.glitch.me/",
+	"https://amber-attractive-record.glitch.me/",
+	"https://grizzly-stirring-seer.glitch.me/",
+	"https://amazing-mewing-hellebore.glitch.me/",
+	"https://holy-decorous-mango.glitch.me/",
+	"https://miniature-lofty-pyroraptor.glitch.me/",
+	"https://opalescent-wirehaired-saffron.glitch.me/",
+	"https://sordid-silken-snowstorm.glitch.me/",
+	"https://waiting-foamy-cappelletti.glitch.me/",
+	"https://spotless-fortune-ground.glitch.me/",
+	"https://literate-cobalt-locust.glitch.me/",
+	"https://elderly-standing-anemone.glitch.me/",
+	"https://fluttering-deluxe-roquefort.glitch.me/",
+	"https://alder-glib-basin.glitch.me/",
+	"https://juvenile-overjoyed-meadowlark.glitch.me/",
+	"https://extreme-jumpy-caboc.glitch.me/",
+	"https://cors-proxy1.herokuapp.com/",
+	"https://livecountsio-cors-1.herokuapp.com/",
+	"https://next-is-thief-1.herokuapp.com/",
+	"https://next-is-thief-2.herokuapp.com/",
+	"https://next-is-thief-3.herokuapp.com/",
+	"https://next-is-thief-4.herokuapp.com/",
+	"https://next-is-thief-5.herokuapp.com/",
+	"https://next-is-thief-6.herokuapp.com/",
+	"https://next-is-thief-7.herokuapp.com/",
+	"https://next-is-thief-8.herokuapp.com/",
+	"https://next-is-thief-9.herokuapp.com/",
+	"https://next-is-thief-10.herokuapp.com/",
+	"https://next-is-thief-11.herokuapp.com/"
+]
+
+window.onload = () => {
+    $.getJSON(NextStopStealingMyShitRetard[Math.floor(Math.random() * NextStopStealingMyShitRetard.length)]+'https://www.tiktok.com/node/share/user/@'+user, function(data) {
+        document.querySelector('#user_pic').src = data.body.userData.coversMedium[0]
+        document.querySelector('#name').innerHTML = data.body.userData.nickName
+        document.querySelector('#odometer').innerHTML = data.body.userData.fans
+    })
+}
+
+setInterval(function() {
+    $.getJSON(NextStopStealingMyShitRetard[Math.floor(Math.random() * NextStopStealingMyShitRetard.length)]+'https://www.tiktok.com/node/share/user/@'+user, function(data) {
+        if (!ok) {
+            document.querySelector('#user_pic').src = data.body.userData.coversMedium[0]
+            document.querySelector('#name').innerHTML = data.body.userData.nickName
+            ok = true;  
+        }
+        document.querySelector('#odometer').innerHTML = data.body.userData.fans
+    })
+}, 2000)
+
+//---------------------------------------------------------------//
+//  ______ _    _ _   _  _____ _______ _____ ____  _   _  _____  //
+// |  ____| |  | | \ | |/ ____|__   __|_   _/ __ \| \ | |/ ____| //
+// | |__  | |  | |  \| | |       | |    | || |  | |  \| | (___   //
+// |  __| | |  | | . ` | |       | |    | || |  | | . ` |\___ \  //
+// | |    | |__| | |\  | |____   | |   _| || |__| | |\  |____) | //
+// |_|     \____/|_| \_|\_____|  |_|  |_____\____/|_| \_|_____/  //
+//                                                        		 //
+//---------------------------------------------------------------//                                                            
+
+var user = "";
+
+if (getUrlVars()["t"] == "1") {
+	document.getElementById('style').href='../../../assets/global/odometer-fast.css';
+}
+
+if (getUrlVars()["o"] == "1" && getUrlVars()["t"] == "1") {
+    $('head').append('<link rel="stylesheet" type="text/css" href="/assets/global/odometer-2.css">');
+    document.getElementById('style').href='../../../assets/global/odometer-fast.css';
+}
+
+if (getUrlVars()["o"] == "1") {
+	document.getElementById('style').href='../../../assets/global/odometer-fast.css';
+}
+
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value
+    });
+    return vars
+}
+
+if (!getUrlVars()["u"]) {
+    $(document).ready(function() {
+        if (window.location.href.indexOf("?") > -1) {
+            history.pushState(null, '', window.location.href + '&u=' + user);
+        } else {
+            history.pushState(null, '', window.location.href + '?u=' + user);
+        }
+    });
+}
+
+if (!getUrlVars()["o"]) {
+	$(document).ready(function() {
+        if (window.location.href.indexOf("?") > -1) {
+            history.pushState(null, '', window.location.href + '&o=0');
+        } else {
+            history.pushState(null, '', window.location.href + '?o=0');
+        }
+    });
+}
+
+if (!getUrlVars()["u"]) {
+	user = "charlidamelio";
+} else {
+	user = getUrlVars()["u"];
+}
