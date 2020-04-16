9cdce00567dc449cb9de86c5cc3542e592497002
Shazyy <yarukemc@gmail.com>
2020-04-16 15:50:26 +0200

    even more testing bruh

--- assets/global/corsProxies.js
+++ assets/global/corsProxies.js
@@ -1,22 +1,13 @@
 var corsProxies = [
     "https://kaput-classic-explanation.glitch.me/",
-        "https://upbeat-grandiose-fortnight.glitch.me/",
-    "https://sparkly-marbled-acorn.glitch.me/",
-    "https://flat-flannel-hockey.glitch.me/",
-    "https://pointed-glacier-variraptor.glitch.me/",
-    "https://standing-nonstop-kilogram.glitch.me/",
-    "https://astonishing-discovered-people.glitch.me/",
-    "https://fluffy-atom-minnow.glitch.me/",
-    "https://polished-truthful-care.glitch.me/",
-    "https://treasure-healthy-booklet.glitch.me/",
-    "https://storm-sky-beach.glitch.me/",
-    "https://suave-luck-mine.glitch.me/",
-    "https://polar-developing-road.glitch.me/",
-    "https://loud-feline-cardboard.glitch.me/",
-    "https://awesome-morning-ceder.glitch.me/",
-    "https://frill-unleashed-peacock.glitch.me/",
-    "https://admitted-gelatinous-gerbera.glitch.me/",
-    "https://hot-cultured-fontina.glitch.me/",
-    "https://calm-kindhearted-lock.glitch.me/"
-
+    "https://kaput-classic-explanation.glitch.me/",
+    "https://magnificent-mint-dog.glitch.me/",
+    "https://efficacious-resisted-shear.glitch.me/",
+    "https://magnetic-lucky-gecko.glitch.me/",
+    "https://feather-bald-hawk.glitch.me/",
+    "https://animated-fast-wolfberry.glitch.me/",
+    "https://calico-guttural-feather.glitch.me/",
+    "https://fallacious-agreeable-filament.glitch.me/",
+    "https://wise-daffodil-brisket.glitch.me/",
+    "https://ossified-polydactyl-sprout.glitch.me/"    
 ]
