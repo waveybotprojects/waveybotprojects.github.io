feba6d199bb67cb349ef14748d9b0cc3b5bac96c
Shazyy <yarukemc@gmail.com>
2020-04-16 14:24:15 +0200

    tiny test

--- assets/global/corsProxies.js
+++ assets/global/corsProxies.js
@@ -1,3 +1,22 @@
 var corsProxies = [
-    "https://kaput-classic-explanation.glitch.me/"
+    "https://kaput-classic-explanation.glitch.me/",
+        "https://upbeat-grandiose-fortnight.glitch.me/",
+    "https://sparkly-marbled-acorn.glitch.me/",
+    "https://flat-flannel-hockey.glitch.me/",
+    "https://pointed-glacier-variraptor.glitch.me/",
+    "https://standing-nonstop-kilogram.glitch.me/",
+    "https://astonishing-discovered-people.glitch.me/",
+    "https://fluffy-atom-minnow.glitch.me/",
+    "https://polished-truthful-care.glitch.me/",
+    "https://treasure-healthy-booklet.glitch.me/",
+    "https://storm-sky-beach.glitch.me/",
+    "https://suave-luck-mine.glitch.me/",
+    "https://polar-developing-road.glitch.me/",
+    "https://loud-feline-cardboard.glitch.me/",
+    "https://awesome-morning-ceder.glitch.me/",
+    "https://frill-unleashed-peacock.glitch.me/",
+    "https://admitted-gelatinous-gerbera.glitch.me/",
+    "https://hot-cultured-fontina.glitch.me/",
+    "https://calm-kindhearted-lock.glitch.me/"
+
 ]
